4ca69e8d29b90ea95c621fb8d606deed40169f43
Arjunsingh Gautam <arjunsingh.gautam.01@gmail.com>
2026-05-14 16:55:59 +0530

    Pydantic in FastAPI

--- Lesson 34 FastAPI/fastapi_blog/main.py
+++ Lesson 34 FastAPI/fastapi_blog/main.py
@@ -4,6 +4,7 @@ from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from .schemas import PostCreate,PostResponse
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -45,17 +46,38 @@ def post(request:Request,post_id:int):
 
 
 # JSON APIs
-@app.get("/api/posts")
+
+# GET APIs
+@app.get("/api/posts",response_model=list[PostResponse])
 def get_posts():
     return posts
 
-@app.get("/api/posts/{post_id}")
+@app.get("/api/posts/{post_id}",response_model=PostResponse)
 def get_post(post_id:int):
     for post in posts:
         if post.get("id")==post_id:
             return post
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Post not Found!")
 
+
+# POST APIs:
+
+@app.post("/api/posts",response_model=PostResponse,status_code=status.HTTP_201_CREATED)
+def create_post(post:PostCreate):
+    new_id=max(p['id'] for p in posts)+1 if posts else 1
+    new_post={
+        "id":new_id,
+        "author":post.author,
+        "title":post.title,
+        "content":post.content,
+        "date_posted":"Aprile 23,2026"
+    }
+    posts.append(new_post)
+    return new_post
+
+
+
+# Exception Handlers
 @app.exception_handler(StarletteHTTPException)
 def general_http_exception_handler(request: Request, exception: StarletteHTTPException):
     message = (
